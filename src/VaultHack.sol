463c2abefdb8c2b2982dc115899734ebb62edecc
marcosbroc <mbroc@ontarget21.co.uk>
2026-06-03 23:20:54 +0200

    Vault unlocked

---
+++ src/VaultHack.sol
@@ -0,0 +1,6 @@
+/**
+ *
+ * > cast storage ${VAULT_INSTANCE_ADDRESS} 1 --rpc-url ${SEPOLIA_RPC_URL}
+ * > cast send ${VAULT_INSTANCE_ADDRESS} "unlock(bytes32)" ${VAULT_HEX_RESULT} --account updraft --rpc-url ${SEPOLIA_RPC_URL}
+ *
+ */
